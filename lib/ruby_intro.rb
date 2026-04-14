f9d76116e271a1105602e5944818b3bf02da51e4
AMindToThink <61801493+AMindToThink@users.noreply.github.com>
2026-04-13 23:29:28 -0500

    part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,35 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  arr.each{|e| sum += e}
+  sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  end
+  biggest = nil
+  biggest_i = nil
+  second_big = nil
+  arr.each_with_index do |e, i| 
+    if (biggest == nil || biggest < e)
+      biggest =  e
+      biggest_i = i
+    end
+  end
+  arr.each_with_index{|e, i| second_big  = i != biggest_i && (second_big == nil || second_big < e)  ? e : second_big}
+  return biggest + second_big
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  arr.each_with_index.any?{|e, i| arr[(i+1)..-1].any?{|e2| e + e2 == n}}
+
 end
 
 # Part 2
